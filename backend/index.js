f3c02911599a5d0eb98df2ec7d6a385deec808c4
Yash-Dhakar <yash19me137@satiengg.in>
2024-06-28 21:46:49 +0530

    User Model Creation

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app=express();
-app.listen(3000,()=>{
-    console.log("Server running on port no 3000!!")
-})
+// Load environment variables from .env file
+dotenv.config();
+
+
+
+const app = express();
+
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/real-estate").then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on port 3000!!');
+});
